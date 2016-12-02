8c1744bf7685894ced4013f215427deb078cb5aa
Jeremy <jrmullen@oakland.edu>
2016-12-02 00:05:06 -0500

    day02 structure created

--- src/main/java/Day01/Day01.java
+++ src/main/java/Day01/Day01.java
@@ -25,10 +25,16 @@ public class Day01 {
         int x = 0;
         int y = 0;
         int degrees = 90;
+        ArrayList setList = new ArrayList();
+        ArrayList doubleVisit = new ArrayList();
 
         for(String step : instructions) {
             String direction = step.substring(0, 1);
             int distance = Integer.parseInt(step.substring(1, step.length()));
+            int[] set = new int[2];
+
+//            set[0] = x;
+//            set[1]  =y;
 
             if (direction.equals("L")) {
                 degrees += 90;
@@ -38,18 +44,50 @@ public class Day01 {
 
             switch (degrees % 360) {
                 case 0:
+                    for (int i=0; i<=distance; i++){
+                        set[0] = x + i;
+                        set[1] = y;
+                        if (setList.contains(set)){
+                            System.out.println("set " + set[0] + ", " + set[1] + " visited again");
+                        }
+                        setList.add(set);
+                    }
                     x += distance;
                     break;
                 case 90:
                 case -270:
+                    for (int i=0; i<=distance; i++){
+                        set[0] = x;
+                        set[1] = y + i;
+                        if (setList.contains(set)){
+                            System.out.println("set " + set[0] + ", " + set[1] + " visited again");
+                        }
+                        setList.add(set);
+                    }
                     y += distance;
                     break;
                 case 180:
                 case -180:
+                    for (int i=0; i<=distance; i++){
+                        set[0] = x - i;
+                        set[1] = y;
+                        if (setList.contains(set)){
+                            System.out.println("set " + set[0] + ", " + set[1] + " visited again");
+                        }
+                        setList.add(set);
+                    }
                     x -= distance;
                     break;
                 case 270:
                 case -90:
+                    for (int i=0; i<=distance; i++){
+                        set[0] = x;
+                        set[1] = y - i;
+                        if (setList.contains(set)){
+                            System.out.println("set " + set[0] + ", " + set[1] + " visited again");
+                        }
+                        setList.add(set);
+                    }
                     y -= distance;
                     break;
             }
@@ -57,6 +95,7 @@ public class Day01 {
 
         int manhattanLength = Math.abs(x) + Math.abs(y);
         System.out.println("Part 1:\t" + manhattanLength + " blocks");
+        System.out.println("Part 2:\t");
 
     }
 }
---
+++ src/main/java/Day02/Day02.java
@@ -0,0 +1,15 @@
+package Day02;
+
+/**
+ * Created by Jeremy on 12/2/2016.
+ */
+
+public class Day02 {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
