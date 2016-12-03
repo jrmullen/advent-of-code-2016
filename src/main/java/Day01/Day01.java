6d9202d2037b091b20eef50a81e16dd00bea8caf
Jeremy <jrmullen@oakland.edu>
2016-12-02 22:11:21 -0500

    Day02 part 1 completed

--- src/main/java/Day01/Day01.java
+++ src/main/java/Day01/Day01.java
@@ -1,23 +1,52 @@
 package Day01;
 
 
-import jdk.internal.util.xml.impl.Pair;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 /**
  * Created by Jeremy on 12/1/2016.
+ *
+--- Day 1: No Time for a Taxicab ---
+
+ Santa's sleigh uses a very high-precision clock to guide its movements, and the clock's oscillator is regulated by stars. Unfortunately, the stars have been stolen... by the Easter Bunny. To save Christmas, Santa needs you to retrieve all fifty stars by December 25th.
+
+ Collect stars by solving puzzles. Two puzzles will be made available on each day in the advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+
+ You're airdropped near Easter Bunny Headquarters in a city somewhere. "Near", unfortunately, is as close as you can get - the instructions on the Easter Bunny Recruiting Document the Elves intercepted start here, and nobody had time to work them out further.
+
+ The Document indicates that you should start at the given coordinates (where you just landed) and face North. Then, follow the provided sequence: either turn left (L) or right (R) 90 degrees, then walk forward the given number of blocks, ending at a new intersection.
+
+ There's no time to follow such ridiculous instructions on foot, though, so you take a moment and work out the destination. Given that you can only walk on the street grid of the city, how far is the shortest path to the destination?
+
+ For example:
+
+ Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.
+ R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.
+ R5, L5, R5, R3 leaves you 12 blocks away.
+ How many blocks away is Easter Bunny HQ?
+
+ Your puzzle answer was 161.
+
+ The first half of this puzzle is complete! It provides one gold star: *
+
+ --- Part Two ---
+
+ Then, you notice the instructions continue on the back of the Recruiting Document. Easter Bunny HQ is actually at the first location you visit twice.
+
+ For example, if your instructions are R8, R4, R4, R8, the first location you visit twice is 4 blocks away, due East.
+
+ How many blocks away is the first location you visit twice?
  */
 
 public class Day01 {
 
     public static void main(String[] args) throws IOException {
 
-        String filepath = "D:\\IntelliJ IDEA 2016.2.5\\IdeaProjects\\AdventOfCode2016\\src\\main\\java\\Day01\\input.txt";
+        String filepath = "src\\main\\java\\Day01\\input.txt";
         BufferedReader reader = new BufferedReader(new FileReader(new File(filepath)));
         String[] instructions = reader.readLine().split(", ");
         reader.close();
@@ -26,9 +55,8 @@ public class Day01 {
         int y = 0;
         int degrees = 90;
         ArrayList setList = new ArrayList();
-        ArrayList doubleVisit = new ArrayList();
 
-        for(String step : instructions) {
+        for (String step : instructions) {
             String direction = step.substring(0, 1);
             int distance = Integer.parseInt(step.substring(1, step.length()));
             int[] set = new int[2];
@@ -44,10 +72,10 @@ public class Day01 {
 
             switch (degrees % 360) {
                 case 0:
-                    for (int i=0; i<=distance; i++){
+                    for (int i = 0; i <= distance; i++) {
                         set[0] = x + i;
                         set[1] = y;
-                        if (setList.contains(set)){
+                        if (setList.contains(set)) {
                             System.out.println("set " + set[0] + ", " + set[1] + " visited again");
                         }
                         setList.add(set);
@@ -56,10 +84,10 @@ public class Day01 {
                     break;
                 case 90:
                 case -270:
-                    for (int i=0; i<=distance; i++){
+                    for (int i = 0; i <= distance; i++) {
                         set[0] = x;
                         set[1] = y + i;
-                        if (setList.contains(set)){
+                        if (setList.contains(set)) {
                             System.out.println("set " + set[0] + ", " + set[1] + " visited again");
                         }
                         setList.add(set);
@@ -68,10 +96,10 @@ public class Day01 {
                     break;
                 case 180:
                 case -180:
-                    for (int i=0; i<=distance; i++){
+                    for (int i = 0; i <= distance; i++) {
                         set[0] = x - i;
                         set[1] = y;
-                        if (setList.contains(set)){
+                        if (setList.contains(set)) {
                             System.out.println("set " + set[0] + ", " + set[1] + " visited again");
                         }
                         setList.add(set);
@@ -80,10 +108,10 @@ public class Day01 {
                     break;
                 case 270:
                 case -90:
-                    for (int i=0; i<=distance; i++){
+                    for (int i = 0; i <= distance; i++) {
                         set[0] = x;
                         set[1] = y - i;
-                        if (setList.contains(set)){
+                        if (setList.contains(set)) {
                             System.out.println("set " + set[0] + ", " + set[1] + " visited again");
                         }
                         setList.add(set);
@@ -95,7 +123,6 @@ public class Day01 {
 
         int manhattanLength = Math.abs(x) + Math.abs(y);
         System.out.println("Part 1:\t" + manhattanLength + " blocks");
-        System.out.println("Part 2:\t");
 
     }
 }
